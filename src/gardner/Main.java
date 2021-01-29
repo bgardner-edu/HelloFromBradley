3b2ec041e752605ba42dac6dcfd4a1112b48f4fa
Brody Roelofs <u771368366@byui.edu>
2021-01-28 17:07:59 -0700

    Message lol

--- src/gardner/Main.java
+++ src/gardner/Main.java
@@ -3,7 +3,7 @@ package gardner;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello, my name is In a old Branch");
+	    System.out.println("Hello, my name is Bradley");
+        System.out.println("Hello Bradley, my name is Brody");
     }
-    SammyHammy sambo = new SammyHammy();
 }
