dfdac5e49bfba7240e42b2aa493ca740a5a0f6c7
Bozo-Cake <78047932+Bozo-Cake@users.noreply.github.com>
2021-01-30 21:09:34 -0700

    Sam's Commit for Credit

--- src/gardner/Main.java
+++ src/gardner/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("Hello, my name is Bradley");
         System.out.println("Hello Bradley, my name is Brody");
+        SammyHammy sambo = new SammyHammy();
     }
 }
